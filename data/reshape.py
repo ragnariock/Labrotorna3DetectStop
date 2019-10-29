a48e881cc3ece668ee2052d8ba402d20e0a381ee
ragnariock <ostap.salo.80@gmail.com>
2019-10-29 20:54:10 +0200

    Data commit

---
+++ data/reshape.py
@@ -0,0 +1,27 @@
+import re
+import os
+import cv2
+
+num = 1
+
+width = 600
+height = 600
+dim = (width, height)
+
+
+for root, dirs, files in os.walk(".", topdown=False):
+	for name in files:
+		#print(os.path.join(root, name))
+		nameImage = os.path.join(root,name)
+		nameImage = str(nameImage)
+		name_root = str(root)
+		if re.search(r'test_images',name_root):
+			if (r'jpg',nameImage ) or re.search(r'jpeg',nameImage) or re.search(r'png',nameImage) or re.search(r'JPG',nameImage):
+				img = cv2.imread(nameImage)
+
+				resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+				cv2.imwrite('images_test/'+str(num) + '.jpg', resized )
+				num +=1
+
+print(num)
